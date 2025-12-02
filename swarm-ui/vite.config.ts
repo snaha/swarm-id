b9c4ae8a5c8a5a31c709e565dee4d4eed520eb30
Vojtech Simetka <vojtech@simetka.cz>
2025-12-02 16:09:12 +0100

    build: remove unnecessary polyfills

--- swarm-ui/vite.config.ts
+++ swarm-ui/vite.config.ts
@@ -1,20 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		nodePolyfills({
-			protocolImports: true,
-		}),
 	],
-	build: {
-		rollupOptions: {
-			external: [
-				/^vite-plugin-node-polyfills\/.*/,
-			],
-		},
+	optimizeDeps: {
+		exclude: ['@swarm-id/lib'],
 	},
 	ssr: {
 		noExternal: ['carbon-icons-svelte'],
