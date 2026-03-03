79d8c3230bec04cdb0c2f5b6b1889880694a0cac
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-03-03 12:37:19 +0000

    deploy: 7231bcbe7e4db60b7168666f2b3dc42803fb861c

---
+++ demo/pr-195/_app/env.js
@@ -0,0 +1,1 @@
+export const env={"PUBLIC_ID_DOMAIN":""}
---
+++ demo/pr-195/_app/immutable/chunks/BlBCDMGb.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1mob4c0?.base??"/demo/pr-195",e=globalThis.__sveltekit_1mob4c0?.assets??s??"";export{e as a,s as b};
