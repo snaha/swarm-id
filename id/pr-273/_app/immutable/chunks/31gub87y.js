25eb784b28fcfc9be385b0b5ee01a34d6b3d8c69
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-03-27 13:05:45 +0000

    deploy: 4e3f96c3870617d8558ee9ad2c6ed51bf083594c

---
+++ id/pr-273/_app/immutable/chunks/31gub87y.js
@@ -0,0 +1,1 @@
+import{g as o}from"./BNVZrmhu.js";import{r,a as e}from"./DqOZy_tZ.js";import{s as a}from"./VItT4N-l.js";function m(){a.data.appOrigin?o(r(e.CONNECT)):(a.clearTemporaryMasterKey(),o(r(e.HOME)))}export{m as n};
