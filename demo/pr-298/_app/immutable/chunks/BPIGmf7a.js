8afc9e9b2f5edb85b40b423786debadb7c230480
agazso <agazso@users.noreply.github.com>
2026-04-07 15:15:33 +0000

    deploy: 90749916a4b7a062e232c98f20b14d30927088fb

---
+++ demo/pr-298/_app/immutable/chunks/BPIGmf7a.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_q7vufm?.base??"/demo/pr-298",e=globalThis.__sveltekit_q7vufm?.assets??s??"";export{e as a,s as b};
---
+++ id/pr-298/_app/immutable/chunks/BuXTHnRC.js
@@ -0,0 +1,1 @@
+import{g as o}from"./DMsMaScn.js";import{r,a as e}from"./D4pemtwB.js";import{s as a}from"./Buv-2Ktk.js";function m(){a.data.appOrigin?o(r(e.CONNECT)):(a.clearTemporaryMasterKey(),o(r(e.HOME)))}export{m as n};
