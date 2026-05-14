f84904c3820a542e9dc995e64f30f06b3f8d7cba
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-05-14 03:01:02 +0000

    deploy: e2a45ed0eb1b391ae5d767fc726442c038cba17f

---
+++ demo/pr-319/_app/immutable/chunks/DbmgkxFV.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1e2zf0f?.base??"/demo/pr-319",e=globalThis.__sveltekit_1e2zf0f?.assets??s??"";export{e as a,s as b};
---
+++ id/pr-319/_app/immutable/chunks/DZdv7XaE.js
@@ -0,0 +1,1 @@
+import{g as o}from"./D_YYlS3F.js";import{r,a as e}from"./Blck6exV.js";import{s as a}from"./Buv-2Ktk.js";function m(){a.data.appOrigin?o(r(e.CONNECT)):(a.clearTemporaryMasterKey(),o(r(e.HOME)))}export{m as n};
