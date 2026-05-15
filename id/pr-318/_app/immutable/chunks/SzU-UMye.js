2d1458e11e6cd3fa290625fe9019447bed312ee6
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-05-15 11:40:26 +0000

    deploy: 47186ffe7745ab89dc5bcb1a1ab85c92dd632290

---
+++ id/pr-318/_app/immutable/chunks/SzU-UMye.js
@@ -0,0 +1,1 @@
+import{g as o}from"./DVjkIfJX.js";import{r,a as e}from"./CcvFlxhF.js";import{s as a}from"./Buv-2Ktk.js";function m(){a.data.appOrigin?o(r(e.CONNECT)):(a.clearTemporaryMasterKey(),o(r(e.HOME)))}export{m as n};
