f0783f5152934aa62e4ac4476589dcb834391e28
agazso <agazso@users.noreply.github.com>
2026-06-23 11:56:29 +0000

    deploy: 81f1dc4349efffaa78e912865cfab1d7914a623f

---
+++ demo-legacy/pr-367/_app/immutable/chunks/C5WzfErN.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_11swq5u?.base??"/demo-legacy/pr-367",e=globalThis.__sveltekit_11swq5u?.assets??s??"";export{e as a,s as b};
---
+++ demo/pr-367/_app/immutable/chunks/DGQPboDi.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1u4j0wr?.base??"/demo/pr-367",e=globalThis.__sveltekit_1u4j0wr?.assets??s??"";export{e as a,s as b};
