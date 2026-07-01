8df12e626429bd5f99b82008e052fc809722b9be
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-07-01 16:38:09 +0000

    deploy: dcf4eb8f64494e732b57c9abfb092af4885acf32

---
+++ demo-legacy/pr-379/_app/immutable/chunks/DQ8OP42C.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1jx1eu5?.base??"/demo-legacy/pr-379",e=globalThis.__sveltekit_1jx1eu5?.assets??s??"";export{e as a,s as b};
---
+++ demo/pr-379/_app/immutable/chunks/DWZb-Rfn.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_xwqj76?.base??"/demo/pr-379",e=globalThis.__sveltekit_xwqj76?.assets??s??"";export{e as a,s as b};
