038727d64457e0fc52d15db91a197e20499d97b4
agazso <agazso@users.noreply.github.com>
2026-07-01 18:45:14 +0000

    deploy: ccc17fddd9dd7ba5229b928f54bde50e23815241

---
+++ demo/pr-382/_app/immutable/chunks/CGEHkK19.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_s355rr?.base??"/demo/pr-382",e=globalThis.__sveltekit_s355rr?.assets??s??"";export{e as a,s as b};
---
+++ id-legacy/pr-382/_app/immutable/chunks/ChBU0gbt.js
@@ -0,0 +1,1 @@
+import{g as o}from"./BXagDhDQ.js";import{r,a as e}from"./34hhDduh.js";import{s as a}from"./DasCLMX7.js";function m(){a.data.appOrigin?o(r(e.CONNECT)):(a.clearTemporaryMasterKey(),o(r(e.HOME)))}export{m as n};
