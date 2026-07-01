621a73e632bb00c0e256f9d801a0133deeb544f8
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-07-01 17:26:22 +0000

    deploy: a783642382454a7a171838d823c7768dadc5e817

---
+++ demo-legacy/pr-379/_app/immutable/chunks/D2XTVrUJ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1fst76h?.base??"/demo-legacy/pr-379",e=globalThis.__sveltekit_1fst76h?.assets??s??"";export{e as a,s as b};
---
+++ demo/pr-379/_app/immutable/chunks/BWvXeLl6.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_cv8vzu?.base??"/demo/pr-379",e=globalThis.__sveltekit_cv8vzu?.assets??s??"";export{e as a,s as b};
