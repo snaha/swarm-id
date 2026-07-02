af14c2e2565442afd9d7f2a35a73da3dfe66b4ac
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-07-02 10:50:40 +0000

    deploy: cc6d6872a5a531c267acc5032a21f1e2763cf216

---
+++ demo/pr-384/_app/immutable/entry/start.BgSrpg1O.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BosM3Tco.js";export{o as load_css,r as start};
---
+++ id-legacy/pr-384/_app/immutable/entry/start.CPY8Et6J.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/5j8QU07e.js";export{o as load_css,r as start};
