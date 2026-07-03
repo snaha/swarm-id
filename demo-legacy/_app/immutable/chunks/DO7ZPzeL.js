d8e958640402ce94a746e0fc08fc049d4e4d2e6d
agazso <agazso@users.noreply.github.com>
2026-07-03 08:08:07 +0000

    deploy: 8d4ac6f0b3725545cadd41f5b04aec8de15c10f2

---
+++ demo-legacy/_app/immutable/chunks/DO7ZPzeL.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_ofxnen?.base??"/demo-legacy",e=globalThis.__sveltekit_ofxnen?.assets??s??"";export{e as a,s as b};
---
+++ demo/_app/immutable/chunks/BxQ8kal5.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_9l4fq1?.base??"/demo",e=globalThis.__sveltekit_9l4fq1?.assets??s??"";export{e as a,s as b};
