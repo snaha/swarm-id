cc187bac753921905e841d67d2a3ca1dc34be2bf
agazso <agazso@users.noreply.github.com>
2026-07-07 17:30:23 +0000

    deploy: 36d454751665758626fa94f0bac0376c65772a28

---
+++ demo-legacy/pr-454/_app/immutable/chunks/CfP19u4y.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_k2dwuo?.base??"/demo-legacy/pr-454",e=globalThis.__sveltekit_k2dwuo?.assets??s??"";export{e as a,s as b};
---
+++ demo/pr-454/_app/immutable/entry/start.C2utPVPh.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Dsih6ltx.js";export{o as load_css,r as start};
