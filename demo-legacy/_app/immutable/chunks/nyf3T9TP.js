fa8c2ee8636947d57d8970565cdb295fa8b183b4
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-07-08 13:53:43 +0000

    deploy: bba19882cd9f9f1e1ae3a7d507d497d049c3ffea

---
+++ demo-legacy/_app/immutable/chunks/nyf3T9TP.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_qp4k53?.base??"/demo-legacy",e=globalThis.__sveltekit_qp4k53?.assets??s??"";export{e as a,s as b};
---
+++ demo/_app/immutable/chunks/Baxod2Xx.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_mos7dp?.base??"/demo",e=globalThis.__sveltekit_mos7dp?.assets??s??"";export{e as a,s as b};
