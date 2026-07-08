3402faa09cd404a2ab618ac247cdd608162e58b7
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-07-08 09:51:18 +0000

    deploy: f820cf828d4d57024dcd89b92743efbf805aa4f4

---
+++ demo-legacy/pr-442/_app/immutable/chunks/BjVjGvEg.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1faewv0?.base??"/demo-legacy/pr-442",e=globalThis.__sveltekit_1faewv0?.assets??s??"";export{e as a,s as b};
---
+++ id-legacy/pr-442/_app/immutable/chunks/Cxq6Ang4.js
@@ -0,0 +1,1 @@
+import{g as o}from"./Sy62VhF5.js";import{r,a as e}from"./5lTw_vPn.js";import{s as a}from"./DasCLMX7.js";function m(){a.data.appOrigin?o(r(e.CONNECT)):(a.clearTemporaryMasterKey(),o(r(e.HOME)))}export{m as n};
