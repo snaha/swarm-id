d4793268e24d15b1fce2e3b67ad63d1f4717a097
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-07-13 13:05:15 +0000

    deploy: fac8b54564018ff13c795aabaab14d05f8e290bc

---
+++ demo/pr-486/_app/immutable/chunks/DC2MEcLr.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_bguwbc?.base??"/demo/pr-486",e=globalThis.__sveltekit_bguwbc?.assets??s??"";export{e as a,s as b};
---
+++ id/pr-486/_app/immutable/nodes/13.x75RSVc0.js
@@ -0,0 +1,1 @@
+import"../chunks/CSSCTZa6.js";import"../chunks/Do-7Rhha.js";import{P as p}from"../chunks/D77FH7Jc.js";function a(o){p(o,{})}export{a as component};
