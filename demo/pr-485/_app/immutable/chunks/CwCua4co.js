d7c87d0076d0553e859d8063b4441615e0d95c8c
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-07-13 13:01:22 +0000

    deploy: 8378be15b1ea2a7627b4fa126f9cdf6a17c85b2b

---
+++ demo/pr-485/_app/immutable/chunks/CwCua4co.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_w7td12?.base??"/demo/pr-485",t=globalThis.__sveltekit_w7td12?.assets??s??"";export{t as a,s as b};
---
+++ id/pr-485/_app/immutable/entry/start.ZAC-j4fg.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CY_tFAiV.js";export{o as load_css,r as start};
