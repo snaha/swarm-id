120f8af04f108e0b6a6085805d085a2ac9aaa256
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-07-13 13:26:35 +0000

    deploy: de3d2fce88a750158cf7284eb645620e0387bb62

---
+++ demo/pr-489/_app/immutable/chunks/C1KEee0f.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_dlr1xu?.base??"/demo/pr-489",e=globalThis.__sveltekit_dlr1xu?.assets??s??"";export{e as a,s as b};
---
+++ demo/pr-489/_app/immutable/entry/start.1cK2U7sk.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BJy9LCS_.js";export{o as load_css,r as start};
