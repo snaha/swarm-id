a36dbadded80d283e7c55a385ceb900b8f61044e
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-07-13 12:19:24 +0000

    deploy: 1d4a3bab736e8353bbf11b147ff0df78994cb3c8

---
+++ demo/pr-481/_app/immutable/chunks/CxCLecuN.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_x86fx5?.base??"/demo/pr-481",e=globalThis.__sveltekit_x86fx5?.assets??s??"";export{e as a,s as b};
---
+++ id/pr-481/_app/immutable/chunks/Do-7Rhha.js
@@ -0,0 +1,1 @@
+import{A as a}from"./BeCb-HGq.js";a();
