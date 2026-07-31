8cd45ed07b7030466fc82785c81f0a35fc0c60b6
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-07-31 23:01:58 +0000

    deploy: 767f9d71011b0f10a027e2353a5b1241b0df8f42

---
+++ demo/pr-511/_app/immutable/chunks/B2p--uDX.js
@@ -0,0 +1,1 @@
+import{i as a}from"./r3F_vQeK.js";a();
---
+++ id/pr-511/_app/immutable/nodes/13.TpynDGFM.js
@@ -0,0 +1,1 @@
+import"../chunks/SyOLmi6z.js";import"../chunks/DHcDez8E.js";import{P as p}from"../chunks/BGmZxuZ8.js";function a(o){p(o,{})}export{a as component};
