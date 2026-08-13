987d6a0a38998943be9bb829a051948b4a20d4aa
agazso <agazso@users.noreply.github.com>
2026-08-13 15:50:43 +0000

    deploy: 79bdd78119bd2a413e6308f6d60fd380f2008fc3

---
+++ demo/pr-530/_app/immutable/chunks/D9CM2Qmb.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_kota8t?.base??"/demo/pr-530",t=globalThis.__sveltekit_kota8t?.assets??s??"";export{t as a,s as b};
---
+++ demo/pr-530/_app/immutable/entry/start.D5X-BES-.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DXVqb4LS.js";export{o as load_css,r as start};
