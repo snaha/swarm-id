1ff777411bb5edd0971cd34269360637555763f8
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-08-25 15:45:25 +0000

    deploy: 7a340643c50263eb07b2e34a433ed0d1ec5ecfc3

---
+++ demo/pr-591/_app/immutable/chunks/DT_ALo9_.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_hfw5rt?.base??"/demo/pr-591",t=globalThis.__sveltekit_hfw5rt?.assets??s??"";export{t as a,s as b};
---
+++ id/pr-591/_app/immutable/nodes/13.34vS3dGs.js
@@ -0,0 +1,1 @@
+import"../chunks/Bd-o_GX5.js";import"../chunks/CcCuG7Ce.js";import{P as p}from"../chunks/CkCHaEou.js";function a(o){p(o,{})}export{a as component};
