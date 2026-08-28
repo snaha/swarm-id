a61c3cde766f97dbf421df8be78de14291259d2b
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-08-28 20:18:13 +0000

    deploy: e4c1bace8fe423c4ac7fa820227dd4359a04bde4

---
+++ demo/pr-534/_app/immutable/chunks/C2Lq-6ex.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1qwlogk?.base??"/demo/pr-534",e=globalThis.__sveltekit_1qwlogk?.assets??s??"";export{e as a,s as b};
---
+++ id/pr-534/_app/immutable/chunks/BFeG3iVu.js
@@ -0,0 +1,1 @@
+const n=globalThis.__sveltekit_1q95ek3.env;function e(){return n.PUBLIC_BUS_SIGNALING_URL||void 0}export{e as b};
