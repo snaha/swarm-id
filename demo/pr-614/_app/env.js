ad58e9e849a6e05ca66c7db294717256fce6e315
agazso <agazso@users.noreply.github.com>
2026-08-31 10:01:32 +0000

    deploy: 832a9b9bf6a0a42151b1b09846580d31c9960980

--- demo/pr-614/_app/env.js
+++ demo/pr-614/_app/env.js
@@ -1,1 +1,1 @@
-export const env={"PUBLIC_ID_DOMAIN":"https://swarm.snaha.net/id"}
+export const env={"PUBLIC_ID_DOMAIN":"https://swarm.snaha.net/id/pr-614"}
---
+++ demo/pr-614/_app/immutable/chunks/C0txxENI.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_4hzyu5?.base??"/demo/pr-614",e=globalThis.__sveltekit_4hzyu5?.assets??s??"";export{e as a,s as b};
