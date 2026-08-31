e4aa5158ab816465b7441bb91636ad8d675506bc
agazso <agazso@users.noreply.github.com>
2026-08-31 19:59:22 +0000

    deploy: 46a60e56e86b2369cd1cd4a12008d6e475d073f8

---
+++ demo/_app/immutable/chunks/Dpf81vnU.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_3jyzua?.base??"/demo",a=globalThis.__sveltekit_3jyzua?.assets??s??"";export{a,s as b};
---
+++ id/_app/immutable/chunks/sdt2xtSs.js
@@ -0,0 +1,1 @@
+const n=globalThis.__sveltekit_qjsr5h.env;function e(){return n.PUBLIC_BUS_SIGNALING_URL||void 0}export{e as b};
