58a52ed171bad764a56f7f966dd6930cfbd85efd
agazso <agazso@users.noreply.github.com>
2026-09-02 07:19:32 +0000

    deploy: 4bdc2d01ba7577348f71929dbcd89b4f73f2b0ff

---
+++ demo/_app/immutable/chunks/CGLChcSf.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_urkt1?.base??"/demo",t=globalThis.__sveltekit_urkt1?.assets??s??"";export{t as a,s as b};
---
+++ id/_app/immutable/chunks/7_XkCesV.js
@@ -0,0 +1,1 @@
+const n=globalThis.__sveltekit_p7zw8s.env;function e(){return n.PUBLIC_BUS_SIGNALING_URL||void 0}export{e as b};
