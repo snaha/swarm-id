e1e8bef328e42be2d7e8954c35d639d57ede1ac8
agazso <agazso@users.noreply.github.com>
2026-09-02 10:33:05 +0000

    deploy: 6231c1071e3c4e594b5f104f6ed7e73fdd878b05

---
+++ demo/pr-655/_app/immutable/chunks/BvV6unGZ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_ausy2m?.base??"/demo/pr-655",a=globalThis.__sveltekit_ausy2m?.assets??s??"";export{a,s as b};
---
+++ id/pr-655/_app/immutable/chunks/C151lXbg.js
@@ -0,0 +1,1 @@
+const n=globalThis.__sveltekit_1imihn4.env;function i(){return n.PUBLIC_BUS_SIGNALING_URL||void 0}export{i as b};
