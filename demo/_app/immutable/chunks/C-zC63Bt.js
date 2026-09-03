00e9a0d1353b0db7674106d33acf9b4cfc50a27b
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-09-03 07:58:10 +0000

    deploy: 24ba5f5250a7ff6d321fe844e6ba221e8cfda403

---
+++ demo/_app/immutable/chunks/C-zC63Bt.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_13vhy3f?.base??"/demo",e=globalThis.__sveltekit_13vhy3f?.assets??s??"";export{e as a,s as b};
---
+++ id/_app/immutable/chunks/T8Z6dvXF.js
@@ -0,0 +1,1 @@
+const n=globalThis.__sveltekit_169pz1b.env;function e(){return n.PUBLIC_BUS_SIGNALING_URL||void 0}export{e as b};
