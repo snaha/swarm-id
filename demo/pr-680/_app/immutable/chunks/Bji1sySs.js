69e9e82fbd67cf4d94d6a9356758a831dd1a4576
agazso <agazso@users.noreply.github.com>
2026-09-04 14:53:42 +0000

    deploy: d65bffd41a0a126b7b5c5164aee7bfa7f93987d4

---
+++ demo/pr-680/_app/immutable/chunks/Bji1sySs.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1dnbdcr?.base??"/demo/pr-680",e=globalThis.__sveltekit_1dnbdcr?.assets??s??"";export{e as a,s as b};
---
+++ demo/pr-680/_app/immutable/entry/start.q2HqUKQG.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/JtKl-219.js";export{o as load_css,r as start};
