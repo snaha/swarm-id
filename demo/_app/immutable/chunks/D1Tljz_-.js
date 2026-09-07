a6035e09ed4c8f5cea2384e70b993cffecc7711a
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-09-07 16:05:37 +0000

    deploy: ec8cd87a51e271a8668ac7735877757ec1a566a7

---
+++ demo/_app/immutable/chunks/D1Tljz_-.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1jj6ypc?.base??"/demo",e=globalThis.__sveltekit_1jj6ypc?.assets??s??"";export{e as a,s as b};
---
+++ demo/_app/immutable/entry/start.CnZQPh9j.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DBu-2ZLb.js";export{o as load_css,r as start};
