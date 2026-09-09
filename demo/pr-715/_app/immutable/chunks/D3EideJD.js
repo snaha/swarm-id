942126276b2effe35dfaf6dd380e64b2e9245d6e
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-09-09 14:15:28 +0000

    deploy: f409892bc35cbcfb05a442645acbcaa86e1819d2

---
+++ demo/pr-715/_app/immutable/chunks/D3EideJD.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_16urybb?.base??"/demo/pr-715",e=globalThis.__sveltekit_16urybb?.assets??s??"";export{e as a,s as b};
---
+++ id/pr-715/_app/immutable/entry/start.CkYalj_P.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DEC-yQoO.js";export{o as load_css,r as start};
