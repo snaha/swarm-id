beabc4431056c1f255e0acbb253fedaf680badab
agazso <agazso@users.noreply.github.com>
2026-09-11 12:12:52 +0000

    deploy: 4711a500193ba861d75665349383291089e8044e

---
+++ demo/_app/immutable/chunks/v1PHkAFI.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_c6a378?.base??"/demo",a=globalThis.__sveltekit_c6a378?.assets??s??"";export{a,s as b};
---
+++ id/_app/immutable/chunks/CT9YpmKJ.js
@@ -0,0 +1,1 @@
+const e=globalThis.__sveltekit_cg9oyf.env;export{e};
