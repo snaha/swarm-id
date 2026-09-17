f1d97c8ad3e0c1e47ec6a34eea845fcd2b748122
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-09-17 07:26:43 +0000

    deploy: 51f170c5de5691a837a96a0e6c1dcf8e4eb4f625

---
+++ demo/pr-755/_app/immutable/chunks/BXE9vWcq.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_10frht?.base??"/demo/pr-755",t=globalThis.__sveltekit_10frht?.assets??s??"";export{t as a,s as b};
---
+++ id/pr-755/_app/immutable/nodes/12.BWEmCV24.js
@@ -0,0 +1,1 @@
+import"../chunks/CHxfgIJm.js";import"../chunks/DZntpE-F.js";import{E as o}from"../chunks/CAxaSbTu.js";const r=!1,s=Object.freeze(Object.defineProperty({__proto__:null,prerender:r},Symbol.toStringTag,{value:"Module"}));function _(e){o(e,{status:404})}export{_ as component,s as universal};
