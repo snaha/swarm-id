501b74f81532433a1424a00e1127da63753ed6b8
vojtechsimetka <vojtechsimetka@users.noreply.github.com>
2026-09-17 07:39:20 +0000

    deploy: cadb4edf21dbda53c44f550b3c509d88261d22b8

---
+++ demo/pr-775/_app/immutable/chunks/CsOYbGBJ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_niqwmw?.base??"/demo/pr-775",e=globalThis.__sveltekit_niqwmw?.assets??s??"";export{e as a,s as b};
---
+++ id/pr-775/_app/immutable/chunks/CaTXxghZ.js
@@ -0,0 +1,1 @@
+const e=globalThis.__sveltekit_1uaqid9.env;export{e};
